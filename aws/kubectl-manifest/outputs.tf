2225f0c0628fa0f804ed6e372a4b94fab0ee1d49
fidiego <sayheytofred@gmail.com>
2025-02-03 15:16:11 -0600

    feat: aws/kubectl_manifest: include more outputs

--- aws/kubectl-manifest/outputs.tf
+++ aws/kubectl-manifest/outputs.tf
@@ -2,3 +2,30 @@
 output "install_name" {
   value = var.install_name
 }
+
+output "yaml_body_parsed" {
+  value     = kubectl_manifest.manifest.yaml_body_parsed
+  sensitive = true
+}
+
+output "kind" {
+  value = kubectl_manifest.manifest.kind
+}
+
+output "name" {
+  value = kubectl_manifest.manifest.name
+}
+
+output "namespace" {
+  value = kubectl_manifest.manifest.namespace
+}
+
+output "yaml_cluster" {
+  value     = yamldecode(kubectl_manifest.manifest.yaml_cluster)
+  sensitive = true
+}
+
+output "live_manifest_incluster" {
+  value     = kubectl_manifest.manifest.live_manifest_incluster
+  sensitive = true
+}

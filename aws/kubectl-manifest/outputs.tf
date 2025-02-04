d12a14a0cf1eb66b9fe498e9cc605881db943139
fidiego <sayheytofred@gmail.com>
2025-02-03 18:20:41 -0600

    chore: aws/kubectl_manifest: update readme and outputs

--- aws/kubectl-manifest/outputs.tf
+++ aws/kubectl-manifest/outputs.tf
@@ -1,4 +1,21 @@
-// NOTE(fd): pass-through output - saw issues w/ component w/out outputs
+output "kind" {
+  value = kubectl_manifest.manifest.kind
+}
+
+output "name" {
+  value = kubectl_manifest.manifest.name
+}
+
+output "namespace" {
+  value = kubectl_manifest.manifest.namespace
+}
+
+output "yaml_body_parsed" {
+  value     = kubectl_manifest.manifest.yaml_body_parsed
+  sensitive = true
+}
+
+// TODO(fd): remove this outpu
 output "install_name" {
   value = var.install_name
 }

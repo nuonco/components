d99ee73833752bc8bd3093d72230c1547a4be876
fidiego <sayheytofred@gmail.com>
2025-02-10 20:28:34 -0600

    feat: rds-cluster: use managed password & update tags
    this will create a Secret Manager secret whose arn will be in the output

--- aws/rds-cluster/outputs.tf
+++ aws/rds-cluster/outputs.tf
@@ -5,3 +5,12 @@ output "endpoint" {
 output "address" {
   value = module.db.db_instance_address
 }
+
+output "db_instance_master_user_secret_arn" {
+  value = module.db.db_instance_master_user_secret_arn
+}
+
+output "db_instance_resource_id" {
+  value = module.db.db_instance_resource_id
+}
+
--- aws/rds-cluster/variables.tf
+++ aws/rds-cluster/variables.tf
@@ -1,5 +1,40 @@
-variable "identifier" {
+locals {
+  subnet_ids = split(",", trim(replace(var.subnet_ids, " ", ","), "[]"))
+  tags = {
+    "component.nuon.co/name" = "rds-cluster"
+    "install.nuon.co/id"     = var.nuon_id
+  }
+}
+
+variable "nuon_id" {
+  type        = string
+  description = "The Nuon Install ID ({{ .nuon.install.id }}."
+}
+
+variable "region" {
+  type        = string
+  description = "The AWS region ({{ .nuon.install_stack.outputs.region }}."
+}
+
+# network details
+variable "vpc_id" {
   type = string
+}
+
+variable "subnet_ids" {
+  type        = string
+  description = "Comma-delimited string of subnet ids to be split for use in this tf."
+}
+
+variable "subnet_group_id" {
+  type        = string
+  description = "The RDS subnet group for this RDS Cluster."
+}
+
+# databse details
+variable "identifier" {
+  type        = string
+  description = "Human friendly (ish) identifier for the cluster."
 }
 
 variable "port" {
@@ -13,25 +48,12 @@ variable "instance_class" {
 }
 
 variable "db_name" {
-  type = string
+  type        = string
+  description = "The name of the default database."
 }
 
 variable "db_user" {
-  type = string
+  type        = string
+  description = "The name of the admin user."
 }
 
-variable "db_password" {
-  type = string
-}
-
-variable "subnet_ids" {
-  type = string
-}
-
-variable "security_group" {
-  type = string
-}
-
-variable "vpc_id" {
-  type = string
-}

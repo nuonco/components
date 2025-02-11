460ae59a6a0b102929ac58dab500db7db259a682
fidiego <sayheytofred@gmail.com>
2025-02-10 20:28:34 -0600

    feat: rds-cluster: switch to managed password
    this will create a Secret Manager secret whose arn will be in the output

--- aws/rds-cluster/outputs.tf
+++ aws/rds-cluster/outputs.tf
@@ -5,3 +5,8 @@ output "endpoint" {
 output "address" {
   value = module.db.db_instance_address
 }
+
+output "db_instance_master_user_secret_arn" {
+  value = module.db.db_instance_master_user_secret_arn
+}
+
